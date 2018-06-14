54d9f015efb266a55644809f6f5d06e8b1816243
Andrew Connell <andrewconnell@dyn-10-32-152-200.winet.local>
2018-06-14 10:02:31 +0100

    14/06/2018 AC
    Updates to .Rd files and slight tweaking of R scripts.

--- R/CROPS.R
+++ R/CROPS.R
@@ -1,4 +1,4 @@
-CROPS <- function(data, penalty="CROPS", pen.value, method="PELT", test.stat="Normal", class=TRUE, param.est=TRUE, minseglen, shape, func){
+CROPS <- function(data, penalty="CROPS", pen.value, costfunc="norm.mean", method="PELT", test.stat="Normal", class=TRUE, param.est=TRUE, minseglen, shape, func){
   if(method != "PELT"){stop('CROPS is a valid penalty choice only if method="PELT", please change your method or your penalty.')}
   mu <- mean(data)
   sumstat=cbind(c(0,cumsum(coredata(data))),c(0,cumsum(coredata(data)^2)),cumsum(c(0,(coredata(data)-mu)^2)))
@@ -10,20 +10,19 @@ CROPS <- function(data, penalty="CROPS", pen.value, method="PELT", test.stat="No
     "Poisson" = {stat = "poisson"},
     {stop("Only Normal, Exponential, Gamma and Poisson are valid test statistics")}
   )
-  costfunc = paste0(func, ".", stat)
   
-  out = range_of_penalties(sumstat, cost=costfunc, min_pen=pen.value[1], max_pen=pen.value[2], minseglen=minseglen)
+  out = online.range_of_penalties(sumstat=sumstat, cost=costfunc, min_pen=pen.value[1], max_pen=pen.value[2], minseglen=minseglen)
   
   if(func=="var"){
-    cpttype="variance"
+    ocpttype="variance"
   }else if(func=="meanvar"){
-    cpttype="mean and variance"
+    ocpttype="mean and variance"
   }else{
-    cpttype="mean"
+    ocpttype="mean"
   }
   
   if(class==TRUE){
-      ans = class_input(data=data,cpttype=cpttype, method="PELT", test.stat=test.stat, penalty=penalty, pen.value=pen.value, minseglen=minseglen, param.estimates=param.est, out=out,shape=shape)
+      ans = online.class_input(data=data,ocpttype=ocpttype, method="PELT", test.stat=test.stat, penalty=penalty, pen.value=pen.value, minseglen=minseglen, param.estimates=param.est, out=out,shape=shape)
       if(func=="var"){
         param.est(ans)=c(param.est(ans),mean=mu)
       }
